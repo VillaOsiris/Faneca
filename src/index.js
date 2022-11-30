402c6655dd8179cf80b57cee681a24545eae3a0e
Sergio Costa <sergio20128@hotmail.com>
2022-11-30 14:04:29 +0000

    quick fix deploy

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/faneca">
+    <BrowserRouter basename="/Portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
