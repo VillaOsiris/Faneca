ff5ae9d921cf671e3adfa0a615973e47249a3238
Sergio Costa <sergio20128@hotmail.com>
2022-12-16 16:33:49 +0000

    fixed project links

--- src/pages/work.js
+++ src/pages/work.js
@@ -10,25 +10,37 @@ function Work() {
       <div className="projects">
         <div className="projects__container">
           <div className="project__container">
-            <div className="project">
+            <a
+              className="project"
+              href="https://villaosiris.github.io/HealthPoint/"
+              target="_blank"
+            >
               <div className="proj--1"></div>
-            </div>
+            </a>
           </div>
           <h2 className="project__title">HealthPoint</h2>
         </div>
         <div className="projects__container">
           <div className="project__container">
-            <div className="project">
+            <a
+              className="project"
+              href="https://villaosiris.github.io/Moov/"
+              target="_blank"
+            >
               <div className="proj--2"></div>
-            </div>
+            </a>
           </div>
           <h2 className="project__title">MooV</h2>
         </div>
         <div className="projects__container">
           <div className="project__container">
-            <div className="project">
+            <a
+              className="project"
+              href="https://villaosiris.github.io/Bgamer/"
+              target="_blank"
+            >
               <div className="proj--3"></div>
-            </div>
+            </a>
           </div>
           <h2 className="project__title">Bgamer</h2>
         </div>
