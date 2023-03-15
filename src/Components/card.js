d390fd50b56ea96f410db3696b8bb244329755df
Sergio Costa <sergio20128@hotmail.com>
2023-03-15 15:24:33 +0000

    fix project page add new card comp

---
+++ src/Components/card.js
@@ -0,0 +1,44 @@
+import * as logos from "../assets/logos";
+import { BsGithub } from "react-icons/bs";
+import { MdOpenInNew } from "react-icons/md";
+
+function Card({
+  projImage,
+  logo,
+  title,
+  stack,
+  description,
+  repository,
+  liveCode,
+}) {
+  return (
+    <div className="h-screen flex align-middle">
+      <div className="card--container">
+        <div className="card--content group">
+          <div className="card--cover">{projImage}</div>
+          <h3 className="card--content__title">{title}</h3>
+          <h4 className="card--content__subtitle mb-4">Stack </h4>
+          <div>{stack}</div>
+          <p className="card--content__text mb-4">{description}</p>
+          <div className="card--content__buttons">
+            <a className="card--content__btn" href={repository} target="_blank">
+              CODE <BsGithub />
+            </a>
+            <a className="card--content__btn" href={liveCode} target="_blank">
+              LIVE <MdOpenInNew />
+            </a>
+          </div>
+        </div>
+        <div className="card--footer">
+          <img
+            className="project__logo"
+            src={logo}
+            style={{ height: "4rem" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/workPage.js
+++ src/pages/workPage.js
@@ -1,9 +1,11 @@
-import hpLogo from "../assets/images/HpLogo.png";
-import moovLogo from "../assets/images/MoovLogo.png";
-import bgamerLogo from "../assets/images/BgamerLogo.png";
 import * as logos from "../assets/logos";
-import { BsGithub } from "react-icons/bs";
-import { MdOpenInNew } from "react-icons/md";
+import Card from "../Components/card";
+import hpImage from "../assets/images/hp_v4.png";
+import BgamerImage from "../assets/images/bgamer_v1.png";
+import MoovImage from "../assets/images/moov_v1_clipped.png";
+import HpLogo from "../assets/images/HpLogo.png";
+import BgamerLogo from "../assets/images/BgamerLogo.png";
+import MoovLogo from "../assets/images/MoovLogo.png";
 
 function Work() {
   return (
@@ -14,303 +16,206 @@ function Work() {
         from
         <span className="color--4"> "/me/dev.js"</span>
       </h2>
-
-      {/* //////////////////////////////////////////////////////////////////// */}
-
       <div className="page--content work">
-        <div>
-          <div className="project__container">
-            <div>
-              <article>
-                <img
-                  className="project__logo"
-                  src={hpLogo}
-                  alt="HealthPoint logo"
-                  style={{ height: "4rem" }}
-                />
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-                  assumenda dolorem quisquam debitis sapiente! Facilis
-                  repudiandae cupiditate sed corrupti? Laboriosam neque iusto
-                  cum error ullam, architecto qui sunt. Dolores, perspiciatis!
-                </p>
-                <div className="stack">
-                  <img
-                    src={logos.htmlLogo}
-                    alt="html logo"
-                    width="30px"
-                    height="30px"
-                  />
-                  <img
-                    src={logos.cssLogo}
-                    alt="css logo"
-                    width="30px"
-                    height="30px"
-                  />
-                  <img
-                    src={logos.jsLogo}
-                    alt="js logo"
-                    width="30px"
-                    height="30px"
-                  />
-                </div>
-                <div className="icons">
-                  <a href="https://github.com/VillaOsiris" target="_blank">
-                    CODE <BsGithub className="icons--link" />
-                  </a>
-                  <a href="https://www.linkedin.com/in/sergiocscosta/">
-                    Live Demo <MdOpenInNew className="icons--link" />
-                  </a>
-                </div>
-              </article>
-              <div>
-                <a
-                  className="project"
-                  href="https://villaosiris.github.io/HealthPoint/"
-                  target="_blank"
-                >
-                  <div className="proj--1"></div>
-                </a>
-              </div>
-            </div>
+        <Card
+          projImage={<img src={hpImage} alt="" width="100%" height="100%" />}
+          logo={HpLogo}
+          title={"HealthPoint"}
+          repository={"https://github.com/VillaOsiris/HealthPoint"}
+          liveCode={"https://villaosiris.github.io/HealthPoint/"}
+          stack={
+            <>
+              <img
+                src={logos.htmlLogo}
+                alt="html logo"
+                width="20px"
+                height="20px"
+              />
+              <img
+                src={logos.cssLogo}
+                alt="css logo"
+                width="20px"
+                height="20px"
+              />
+              <img
+                src={logos.jsLogo}
+                alt="js logo"
+                width="20px"
+                height="20px"
+              />
+            </>
+          }
+          description={
+            "Local gym landing page project involves design and development to showcase its facilities, classes, trainers, and membership plans to attract potential customers and increase gym membership."
+          }
+        />
 
-            <div>
-              <article>
-                <img
-                  className="project__logo"
-                  src={bgamerLogo}
-                  alt="Bgamer logo"
-                  style={{ height: "4rem" }}
-                />
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-                  assumenda dolorem quisquam debitis sapiente! Facilis
-                  repudiandae cupiditate sed corrupti? Laboriosam neque iusto
-                  cum error ullam, architecto qui sunt. Dolores, perspiciatis!
-                </p>
-                <div className="stack">
-                  <img
-                    src={logos.htmlLogo}
-                    alt="html logo"
-                    width="30px"
-                    height="30px"
-                  />
-                  <img
-                    src={logos.cssLogo}
-                    alt="css logo"
-                    width="30px"
-                    height="30px"
-                  />
-                  <img
-                    src={logos.jsLogo}
-                    alt="js logo"
-                    width="30px"
-                    height="30px"
-                  />
-                </div>
-                <div className="icons">
-                  <a href="https://github.com/VillaOsiris" target="_blank">
-                    CODE <BsGithub className="icons--link" />
-                  </a>
-                  <a href="https://www.linkedin.com/in/sergiocscosta/">
-                    Live Demo <MdOpenInNew className="icons--link" />
-                  </a>
-                </div>
-              </article>
-              <div>
-                <a
-                  className="project"
-                  href="https://villaosiris.github.io/Bgamer/"
-                  target="_blank"
-                >
-                  <div className="proj--3"></div>
-                </a>
-              </div>
-            </div>
+        <Card
+          projImage={
+            <img src={BgamerImage} alt="" width="100%" height="100%" />
+          }
+          logo={BgamerLogo}
+          title={"Bgamer"}
+          repository={"https://github.com/VillaOsiris/Bgamer"}
+          liveCode={"https://villaosiris.github.io/Bgamer/"}
+          stack={
+            <>
+              <img
+                src={logos.htmlLogo}
+                alt="html logo"
+                width="20px"
+                height="20px"
+              />
+              <img
+                src={logos.cssLogo}
+                alt="css logo"
+                width="20px"
+                height="20px"
+              />
+              <img
+                src={logos.jsLogo}
+                alt="js logo"
+                width="20px"
+                height="20px"
+              />
+            </>
+          }
+          description={
+            "E-commerce that involves an online store that specializes in selling video games. User-friendly interface that allows customers to browse and purchase games, sign-in and manage their cart history."
+          }
+        />
 
-            <div>
-              <article>
-                <img
-                  className="project__logo"
-                  src={moovLogo}
-                  alt="HealthPoint logo"
-                  style={{ height: "4rem" }}
-                />
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-                  assumenda dolorem quisquam debitis sapiente! Facilis
-                  repudiandae cupiditate sed corrupti? Laboriosam neque iusto
-                  cum error ullam, architecto qui sunt. Dolores, perspiciatis!
-                </p>
-                <div className="stack">
-                  <img
-                    src={logos.htmlLogo}
-                    alt="html logo"
-                    width="30px"
-                    height="30px"
-                  />
-                  <img
-                    src={logos.cssLogo}
-                    alt="css logo"
-                    width="30px"
-                    height="30px"
-                  />
-                  <img
-                    src={logos.jsLogo}
-                    alt="js logo"
-                    width="30px"
-                    height="30px"
-                  />
-                </div>
-                <div className="icons">
-                  <a href="https://github.com/VillaOsiris" target="_blank">
-                    CODE <BsGithub className="icons--link" />
-                  </a>
-                  <a href="https://www.linkedin.com/in/sergiocscosta/">
-                    Live Demo <MdOpenInNew className="icons--link" />
-                  </a>
-                </div>
-              </article>
-              <div>
-                <a
-                  className="project"
-                  href="https://villaosiris.github.io/HealthPoint/"
-                  target="_blank"
-                >
-                  <div className="proj--2"></div>
-                </a>
-              </div>
-            </div>
+        <Card
+          projImage={<img src={MoovImage} alt="" width="100%" height="100%" />}
+          title={"MooV"}
+          logo={MoovLogo}
+          repository={"https://github.com/VillaOsiris/Moov"}
+          liveCode={"https://villaosiris.github.io/Moov/"}
+          stack={
+            <>
+              <img
+                src={logos.htmlLogo}
+                alt="html logo"
+                width="20px"
+                height="20px"
+              />
+              <img
+                src={logos.cssLogo}
+                alt="css logo"
+                width="20px"
+                height="20px"
+              />
+              <img
+                src={logos.jsLogo}
+                alt="js logo"
+                width="20px"
+                height="20px"
+              />
+            </>
+          }
+          description={
+            "Responsive movie streaming platform project involves building a website that allows users to watch movies online.Fully responsive with Rest API integration and enhanced user experience."
+          }
+        />
 
-            <a
-              className="project"
-              href="https://villaosiris.github.io/HealthPoint/"
-              target="_blank"
-            >
-              <div className="proj--1"></div>
-            </a>
-            <img
-              className="project__logo"
-              src={hpLogo}
-              alt="HealthPoint logo"
-              style={{ height: "5rem" }}
-            />
-          </div>
+        <Card
+          projImage={
+            <img src={null} alt="COMING SOON" width="100%" height="100%" />
+          }
+          title={"Title"}
+          logo={""}
+          repository={"#"}
+          liveCode={"#"}
+          stack={
+            <>
+              <img
+                src={logos.htmlLogo}
+                alt="html logo"
+                width="20px"
+                height="20px"
+              />
+              <img
+                src={logos.cssLogo}
+                alt="css logo"
+                width="20px"
+                height="20px"
+              />
+              <img
+                src={logos.jsLogo}
+                alt="js logo"
+                width="20px"
+                height="20px"
+              />
+            </>
+          }
+          description={"COMING SOON!"}
+        />
 
-          {/* <div className="project__highlights">
-            <h3>Project Highlights</h3>
-            <ul>
-              <li>Service Landing Page</li>
-              <li>Creative Design</li>
-              <li>React Router</li>
-              <li>Sass</li>
-            </ul>
-          </div> */}
-        </div>
-        {/* <div>
-          <div className="project__container">
-            <a
-              className="project"
-              href="https://villaosiris.github.io/Moov/"
-              target="_blank"
-            >
-              <div className="proj--2"></div>
-            </a>
-            <img
-              className="project__logo"
-              src={moovLogo}
-              alt="Moov logo"
-              style={{ height: "4rem" }}
-            />
-          </div>
-          <div className="project__highlights">
-            <h3>Project Highlights</h3>
-            <ul>
-              <li>Movies Database</li>
-              <li>REST API integration</li>
-              <li>Condicional Toggle</li>
-              <li>Responsive Design</li>
-            </ul>
-          </div>
-        </div>
-        <div>
-          <div className="project__container">
-            <a
-              className="project"
-              href="https://villaosiris.github.io/Bgamer/"
-              target="_blank"
-            >
-              <div className="proj--3"></div>
-            </a>
-            <img
-              className="project__logo"
-              src={bgamerLogo}
-              alt="Bgamer logo"
-              style={{ height: "4rem" }}
-            />
-          </div>
-          <div className="project__highlights">
-            <h3>Project Highlights</h3>
-            <ul>
-              <li>E-commerce Shop</li>
-              <li>Context Hooks / Redux</li>
-              <li>Data formating and Filtering</li>
-              <li>Styled Components</li>
-            </ul>
-          </div>
-        </div>
-        <div>
-          <div className="project__container">
-            <a
-              className="project"
-              href="https://villaosiris.github.io/Bgamer/"
-              target="_blank"
-            >
-              <div className="proj--3"></div>
-            </a>
-            <img
-              className="project__logo"
-              src={bgamerLogo}
-              alt="Bgamer logo"
-              style={{ height: "4rem" }}
-            />
-          </div>
-          <div className="project__highlights">
-            <h3>Project Highlights</h3>
-            <ul>
-              <li>E-commerce Shop</li>
-              <li>Context Hooks / Redux</li>
-              <li>Data formating and Filtering</li>
-              <li>Styled Components</li>
-            </ul>
-          </div>
-        </div> */}
-        {/* <div>
-          {/* <div className="project__container">
-            <a
-              className="project"
-              href="https://villaosiris.github.io/Bgamer/"
-              target="_blank"
-            >
-              <div className="proj--3"></div>
-            </a>
-            <img
-              className="project__logo"
-              src={bgamerLogo}
-              alt="Bgamer logo"
-              style={{ height: "4rem" }}
-            />
-          </div> */}
-        {/* <div className="project__highlights">
-            <h3>Project Highlights</h3>
-            <ul>
-              <li>E-commerce Shop</li>
-              <li>Context Hooks / Redux</li>
-              <li>Data formating and Filtering</li>
-              <li>Styled Components</li>
-            </ul>
-          </div> 
-        </div> */}
+        <Card
+          projImage={
+            <img src={null} alt="COMING SOON" width="100%" height="100%" />
+          }
+          title={"Title"}
+          logo={""}
+          repository={"#"}
+          liveCode={"#"}
+          stack={
+            <>
+              <img
+                src={logos.htmlLogo}
+                alt="html logo"
+                width="20px"
+                height="20px"
+              />
+              <img
+                src={logos.cssLogo}
+                alt="css logo"
+                width="20px"
+                height="20px"
+              />
+              <img
+                src={logos.jsLogo}
+                alt="js logo"
+                width="20px"
+                height="20px"
+              />
+            </>
+          }
+          description={"COMING SOON!"}
+        />
+
+        <Card
+          projImage={
+            <img src={null} alt="COMING SOON" width="100%" height="100%" />
+          }
+          title={"Title"}
+          logo={""}
+          repository={"#"}
+          liveCode={"#"}
+          stack={
+            <>
+              <img
+                src={logos.htmlLogo}
+                alt="html logo"
+                width="20px"
+                height="20px"
+              />
+              <img
+                src={logos.cssLogo}
+                alt="css logo"
+                width="20px"
+                height="20px"
+              />
+              <img
+                src={logos.jsLogo}
+                alt="js logo"
+                width="20px"
+                height="20px"
+              />
+            </>
+          }
+          description={"COMING SOON!"}
+        />
       </div>
     </div>
   );
